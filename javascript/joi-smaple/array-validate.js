70bfbae6716901a5d04c49fab37d2423539dcc66
vincentliu99999 <vincentliu99999@gmail.com>
2019-07-02 22:41:06 +0800

    add Joi array of object validate sample

--- javascript/joi-smaple/array-validate.js
+++ javascript/joi-smaple/array-validate.js
@@ -8,3 +8,17 @@ const reslut = array.validate(['a', 'b', 'a'], (err, value) => {
 
  console.log(Joi.describe(array).items);
  
+
+let service = Joi.object().keys({
+  serviceNo: Joi.number().required(),
+  serviceName: Joi.string().required(),
+}).unknown()
+
+let services = Joi.array().items(service);
+
+let test = Joi.validate(
+  [{ serviceNo: 0, serviceName: 'service1' }, { serviceNo: 1, serviceName: 'service2', xxx: 's' }],
+  services,
+)
+
+console.log(test);
