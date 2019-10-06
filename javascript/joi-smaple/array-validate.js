1cb6ffab9d8fa588b58429d1667e276ef84e3078
vincentliu99999 <vincentliu99999@gmail.com>
2019-10-06 23:24:31 +0800

    add console

--- javascript/joi-smaple/array-validate.js
+++ javascript/joi-smaple/array-validate.js
@@ -2,23 +2,54 @@ const Joi = require('@hapi/joi');
 
 const array = Joi.array().items(Joi.string().valid('a', 'b'));
 const reslut = array.validate(['a', 'b', 'a'], (err, value) => {
-  console.log(err);
-  console.log(value);
+  // console.log(err);
+  // console.log(value);
  });
 
- console.log(Joi.describe(array).items);
- 
+// console.log(Joi.describe(array).items);
 
-let service = Joi.object().keys({
-  serviceNo: Joi.number().required(),
-  serviceName: Joi.string().required(),
+const bodySchema = Joi.object({
+  unit: Joi.number().min(0).max(1).required(),
+  price: Joi.number().min(100).max(1000).required(),
+  exp: Joi.number().min(0).max(11).required(),
+  area: Joi.array().items(Joi.number()).required(),
+  onsiteOpts: Joi.array().items(Joi.number().min(0).max(2)).required(),
+  clientCats: Joi.array().items(Joi.number().min(0).max(5)).required(),
+  priority: Joi.array().items(Joi.number().min(1).max(6)).required(),
+  desc: Joi.string().allow(''),
+}).required()
+
+let service = Joi.object({
+  id: Joi.number().required(),
+  title: Joi.string().required(),
+  cats: Joi.array().items(Joi.number()).required(),
+  body: bodySchema,
+  catTag: Joi.array().items(Joi.string()).required(),
 }).unknown()
 
-let services = Joi.array().items(service);
+const serviceData = [
+  { id: 0, 
+    title: 'service1',
+    cats: [123, 456],
+    body: {
+      unit: 0,
+      price: 999,
+      exp: 5,
+      area: [123, 456],
+      onsiteOpts: [0, 1, 2],
+      clientCats: [0, 1, 2, 3, 4, 5],
+      priority: [1, 2, 3, 4, 5, 6],
+      desc: '',
+    },
+    catTag: [],
+  },
+  { id: 1, title: 'service2', xxx: 's' }
+];
 
-let test = Joi.validate(
-  [{ serviceNo: 0, serviceName: 'service1' }, { serviceNo: 1, serviceName: 'service2', xxx: 's' }],
-  services,
-)
-
+let test = Joi.validate(serviceData[0], service)
 console.log(test);
+
+
+let services = Joi.array().items(service);
+test = Joi.validate(serviceData, services)
+// console.log(test);
--- javascript/joi-smaple/date-validate.js
+++ javascript/joi-smaple/date-validate.js
@@ -7,6 +7,9 @@ const dates = [
   moment().subtract(16, 'years').toISOString(),
   '1987-06-11',
 ]
+
+console.log(`date should less than ${moment().subtract(15, 'years').format()}`);
+
 const dateSchema = Joi.date().iso().less(moment().subtract(15, 'years').format());
 
 dates.forEach((date, i) => {
